e3e3b86f342fa9cefce0d6ee692b2b9ac8a9eb8b
telmed_monica <mlieng@ucdavis.edu>
2019-03-14 22:17:57 -0700

    2019/03/14 Recreate normalize & recode variables #3

---
+++ 1.1_supporting_functions.R
@@ -0,0 +1,323 @@
+#' This function copies the 'csv' and imports into the PostgreSQL database
+#' Only imports a subset to reduce overall size of database
+#' Originally developed in 2019-12-04 Load Database 3
+db_copy_and_subset = function(file, oshpd_folder, 
+                              column_designations,
+                              destination_table = 'pdd_peds',
+                              unified_col_order = "*",
+                              subset_command = "agyradm<27",
+                              diagnostic=TRUE
+){
+  # create new temp table using format from header
+  suppress_out = dbExecute(con, paste("CREATE TABLE temp_table (",column_designations,")"))
+  before.records = dbGetQuery(con, "SELECT COUNT(*) from temp_table")
+  
+  # read data from year----
+  start.time = Sys.time()
+  command = paste0("COPY temp_table FROM '",
+                   file.path(oshpd_folder,file),
+                   "' DELIMITER ',' CSV Header;")
+  if(diagnostic) print(command)
+  suppress_out = dbExecute(con, command)
+  
+  end_time = Sys.time()
+  if(diagnostic) print(end_time - start.time)
+  if(diagnostic) after.records = dbGetQuery(con, "SELECT COUNT(*) from temp_table")
+  if(diagnostic) print(cbind(before = before.records[1,1] %>% as.integer(), 
+                             after=after.records[1,1] %>% as.integer()))
+  
+  # copy peds data from year----
+  start.time = Sys.time()
+  before.peds.number = dbGetQuery(
+    con, paste("SELECT COUNT(*) from", destination_table))
+  command = paste("INSERT INTO",destination_table,
+                  "SELECT",unified_col_order,
+                  "FROM temp_table WHERE", subset_command)
+  if (diagnostic) print (command)
+  dbExecute(con, command)
+  end_time = Sys.time()
+  if(diagnostic) print(end_time - start.time)
+  after.peds.number = dbGetQuery(
+    con, paste("SELECT COUNT(*) from", destination_table))
+  if(diagnostic) print(cbind(before = before.peds.number[1,1] %>% as.integer(), 
+                             after=after.peds.number[1,1] %>% as.integer()))
+  
+  # reset/clear temp table----
+  suppress_out = dbExecute(con, "DROP TABLE temp_table")
+}
+
+
+#' Developed in Normalize #2
+#' Recode a variable in PostgreSQL, similar to dplyr::recode
+
+db_recode = function(table, column, code_map){
+  table_col = paste0(table,'.',column)
+  dbWriteTable(con, 'temp_table', code_map)
+  command = paste('UPDATE',table, 'SET', column, 
+                  '= temp_table.to FROM temp_table WHERE', table_col, 
+                  '= temp_table.from' )
+  dbExecute(con, command)
+  out = dbExecute(con, 'DROP table temp_table') # suppresses output
+  return(out)
+}
+
+#' Developed in Normalize #2
+#' Can be used to compare recoded variables
+db_compare_two_cols = function(table, col1, col2){
+  res = dbGetQuery(
+    con, paste("SELECT",col1,", count(",col1,") FROM ",table," GROUP BY", col1))
+  res2 = dbGetQuery(
+    con, paste("SELECT",col2,", count(",col2,") FROM ",table," GROUP BY", col2))
+  cbind(arrange(res,count),arrange(res2, count)) %>% print() 
+}
+list_to_df = function(list_){
+  plyr::ldply(list_,data.frame) %>% `colnames<-`(c("from", "to"))
+}
+
+#' Recodes certains columns in the PD dataset
+#' Inserts in prescribed order into the combined table
+#' Adds columns that are present in the ED data set
+#' Developed 2018/12/19 RenameVars Recode Vars #3
+
+recode_add_PDD_subset <- function(){
+  start.time = Sys.time()
+  # add columns
+  suppress_out=dbExecute(con, "
+                         ALTER TABLE subset_pdd_peds 
+                         ALTER COLUMN sex TYPE TEXT,
+                         ALTER COLUMN admtday TYPE TEXT,
+                         ALTER COLUMN pay_cat TYPE TEXT, 
+                         ADD column database varchar(3),
+                         ADD column insurance TEXT,
+                         ADD column ed_disp INT,
+                         ADD column ed_pr_prin TEXT,
+                         ADD column ed_opr1 TEXT,
+                         ADD column ed_opr2 TEXT,
+                         ADD column ed_opr3 TEXT,
+                         ADD column ed_opr4 TEXT,
+                         ADD column row_n INT,
+                         ADD column db_row_n INT,
+                         ADD column id INT
+                         ")
+  # convert string to date and assign other
+  suppress_out=dbExecute(con, "
+                         UPDATE subset_pdd_peds 
+                         SET admtdate = to_date(admtdate, 'MM/DD/YYYY'),
+                         dschdate = to_date(dschdate,'MM/DD/YYYY'),
+                         bthdate = to_date(bthdate, 'MM/DD/YYYY'),
+                         database = 'pdd',
+                         insurance = pay_cat")
+  # recode
+  suppress_out = db_recode('subset_pdd_peds', 'sex',
+                           code_map=data.frame(
+                             from=c('.','1','2','3','4'),
+                             to=c('Invalid','Male', 'Female', 'Other','Invalid')
+                           ))
+  suppress_out = db_recode('subset_pdd_peds', 'insurance',
+                           code_map=list(
+                             `1` = 'Public-Medicare-MediCal',
+                             `2` = 'Public-Medicare-MediCal', 
+                             `3` = 'Private', 
+                             `4` = 'Other', 
+                             `5` = 'Public-Other',
+                             `6` = 'Public-Other',
+                             `7` = 'Other',
+                             `8` = 'Self-Pay',
+                             `9` = 'Other',
+                             `0` = 'Unknown-Invalid'
+                           ) %>% list_to_df()
+  )
+  suppress_out = db_recode('subset_pdd_peds', 'admtday',
+                           code_map=list(
+                             `1` = 'Sun',
+                             `2` = 'Mon', 
+                             `3` = 'Tue', 
+                             `4` = 'Wed', 
+                             `5` = 'Thu',
+                             `6` = 'Fri',
+                             `7` = 'Sat'
+                           ) %>% list_to_df()
+  )
+  # insertion
+  suppress_out = dbExecute(con, "
+    INSERT INTO combined_peds 
+   SELECT 
+   nextval('record_id_seq'), 
+   ROW_NUMBER() OVER (ORDER BY admtdate) AS db_row_n, database, 
+   rln, sex, bthdate AS birthdate, 
+   agyradm AS age, agyrdsch AS end_age, agdyadm AS age_days,
+   race_grp AS race_group, pls_abbr AS lang_spoken,
+   patzip AS pat_zip, patcnty AS pat_county,
+   pay_cat AS payer, insurance, 
+   oshpd_id, hplzip AS fac_zip, hplcnty as fac_county,
+   typcare AS ip_fac_type,
+   admtdate AS start_date, dschdate AS end_date, admtday AS start_day_of_week,
+   admtyr AS start_year, dsch_yr AS end_year,
+   qtr_adm AS start_quarter, qtr_dsch AS end_quarter,
+   admtmth AS start_month,
+   los, los_adj, charge AS ip_charge, 
+   srcsite AS ip_source_site, srclicns AS ip_source_license, 
+   srcroute AS ip_source_route, admtype AS ip_admission_type,
+   ed_disp, disp AS ip_disp,
+   diag_p AS dx_prin, odiag1 AS odx1, odiag2 AS odx2, odiag3 AS odx3, 
+   odiag4 AS odx4, odiag5 AS odx5,  odiag6 AS odx6,  odiag7 AS odx7,
+   odiag8 AS odx8,  odiag9 AS odx9,  odiag10 AS odx10,
+   poa_p AS dx_poa_prin,
+   ccs_diagp AS ccs_dx_prin, ccs_odiag1 AS ccs_odx1, ccs_odiag2 AS ccs_odx2,
+   ccs_odiag3 AS ccs_odx3, ccs_odiag4 AS ccs_odx4, ccs_odiag5 AS ccs_odx5,
+   ccs_odiag6 AS ccs_odx6, ccs_odiag7 AS ccs_odx7, ccs_odiag8 AS ccs_odx8,
+   ccs_odiag9 AS ccs_odx9, ccs_odiag10 AS ccs_odx10,
+   ecode_p AS ec_prin, ecode1 AS ec1, ecode2 AS ec2, ecode3 AS ec3, ecode4 AS ec4,
+   ed_pr_prin, ed_opr1, ed_opr2, ed_opr3, ed_opr4,
+   epoa_p AS ec_poa_prin, epoa1 AS ec_poa1, epoa2 AS ec_poa2, epoa3 AS ec_poa3, 
+   epoa4 AS ec_poa4,
+   proc_p AS ip_pr_prin, oproc1 AS ip_opr1, oproc2 AS ip_opr2, 
+   oproc3 AS ip_opr3, oproc4 AS ip_opr4, oproc5 AS ip_opr5,
+   proc_pdy AS ip_day_pr_prin, procdy1 AS ip_day_opr1, procdy2 AS ip_day_opr2,
+   procdy3 AS ip_day_opr3, procdy4 AS ip_day_opr4, procdy5 AS ip_day_opr5,
+   mdc AS ip_mdc, msdrg AS ip_msdrg, grouper AS grouper_v, 
+   sev_code AS ip_sev_code, cat_code AS ip_cat_code
+                           
+       FROM subset_pdd_peds")
+  
+  end.time=Sys.time()
+  print(paste('Time Finished: ', end.time))
+  print(end.time-start.time)
+}
+
+
+#' Recodes certains columns in the ED dataset
+#' Inserts in prescribed order into the combined table
+#' Adds columns that are present in the PD data set
+#' Developed 2018/12/19 RenameVars Recode Vars #3
+
+recode_add_EDD_subset <- function(){
+  start.time = Sys.time()
+  # add columns
+  
+  suppress_out=dbExecute(con, "
+                         ALTER TABLE subset_edd_peds
+                         ALTER COLUMN patzip TYPE TEXT,
+                         ALTER column serv_d TYPE TEXT, 
+                         ADD column database varchar(3),
+                         ADD column insurance TEXT,
+                         ADD column los INT,
+                         ADD column los_adj INT,
+                         ADD column ip_source_site INT,
+                         ADD column ip_source_license INT,
+                         ADD column ip_source_route INT,
+                         ADD column ip_admission_type INT,
+                         ADD column ip_charge INT,
+                         ADD column ip_disp INT,
+                         ADD column ip_fac_type INT,
+                         ADD column dx_poa_prin TEXT,
+                         ADD column ec_poa_prin TEXT,
+                         ADD column ec_poa1 TEXT,
+                         ADD column ec_poa2 TEXT,
+                         ADD column ec_poa3 TEXT,
+                         ADD column ec_poa4 TEXT,
+                         ADD column ip_pr_prin TEXT,
+                         ADD column ip_opr1 TEXT,
+                         ADD column ip_opr2 TEXT,
+                         ADD column ip_opr3 TEXT,
+                         ADD column ip_opr4 TEXT,
+                         ADD column ip_opr5 TEXT,
+                         ADD column ip_day_pr_prin INT,
+                         ADD column ip_day_opr1 INT,
+                         ADD column ip_day_opr2 INT,
+                         ADD column ip_day_opr3 INT,
+                         ADD column ip_day_opr4 INT,
+                         ADD column ip_day_opr5 INT,
+                         ADD column ip_mdc INT,
+                         ADD column ip_msdrg INT,
+                         ADD column grouper_v float,
+                         ADD column ip_sev_code INT,
+                         ADD column ip_cat_code TEXT,
+                         ADD column row_n INT,
+                         ADD column db_row_n INT
+                         ")
+  # convert string to date and assign other
+  suppress_out=dbExecute(con, "
+                         UPDATE subset_edd_peds 
+                         SET serv_dt = to_date(serv_dt, 'MM/DD/YYYY'),
+                         brthdate = to_date(brthdate, 'MM/DD/YYYY'),
+                         database = 'edd',
+                         insurance = payer,
+                         los=0")
+  # recode
+  suppress_out = db_recode('subset_edd_peds', 'sex', 
+                           code_map=data.frame(
+                             from = c('M', 'F', 'U','I') , 
+                             to = c('Male', 'Female','Unknown','Invalid')))
+  suppress_out = db_recode('subset_edd_peds', 'insurance',
+                           code_map=list(
+                             `09` = 'Self-Pay',
+                             `11` = 'Other', 
+                             `12` = 'Private', 
+                             `13` = 'Private', 
+                             `14` = 'Private',
+                             `16` = 'Private',
+                             AM = 'Other',
+                             BL = 'Private',
+                             CH = 'Public-Other',
+                             CI = 'Private',
+                             DS = 'Other',
+                             HM = 'Private',
+                             MA = 'Public-Medicare-MediCal',
+                             MB = 'Public-Medicare-MediCal',
+                             MC = 'Public-Medicare-MediCal',
+                             OF = 'Public-Other',
+                             TV = 'Public-Other',
+                             VA = 'Public-Other',
+                             WC = 'Other',
+                             `00` = 'Other',
+                             `99` = 'Unknown-Invalid') %>% list_to_df()
+  )
+  suppress_out = db_recode('subset_edd_peds', 'serv_d',
+                           code_map=list(
+                             `1` = 'Sun',
+                             `2` = 'Mon', 
+                             `3` = 'Tue', 
+                             `4` = 'Wed', 
+                             `5` = 'Thu',
+                             `6` = 'Fri',
+                             `7` = 'Sat'
+                           ) %>% list_to_df()
+  )
+  suppress_out = dbExecute(con, "
+    INSERT INTO combined_peds 
+     SELECT 
+     nextval('record_id_seq'), 
+     ROW_NUMBER() OVER (ORDER BY serv_dt) AS db_row_n, database, 
+     rln, sex, brthdate as birthdate, 
+     agyrserv age, agyrserv end_age, agdyserv AS age_days,
+     race_grp AS race_group, pls_abr AS lang_spoken, 
+     patzip AS pat_zip, patco AS pat_county, 
+     payer, insurance,
+     fac_id AS oshpd_id, faczip AS fac_zip, fac_co AS fac_county, ip_fac_type,
+     serv_dt AS start_date, serv_dt AS end_date, serv_d AS start_day_of_week,
+     serv_y AS start_year, serv_y AS end_year,
+     serv_q AS start_quarter, serv_q AS end_quarter,
+     serv_m AS start_month,
+     los, los_adj, ip_charge, ip_source_site, ip_source_license, ip_source_route,
+     ip_admission_type,
+     
+     dispn AS ed_disp, ip_disp,
+     dx_prin, odx1, odx2, odx3, odx4, odx5, odx6, odx7, odx8, odx9, odx10,
+     dx_poa_prin,
+     ccs_dx_prin, ccs_odx1, ccs_odx2, ccs_odx3, ccs_odx4, ccs_odx5,
+     ccs_odx6, ccs_odx7, ccs_odx8, ccs_odx9, ccs_odx10,
+     ec_prin, ec1, ec2, ec3, ec4,
+     ec_poa_prin, ec_poa1, ec_poa2, ec_poa3, ec_poa4,
+     pr_prin AS ed_pr_prin, opr1 AS ed_opr1, opr2 AS ed_opr2,
+     opr3 AS ed_opr3, opr4 AS ed_opr4,
+     ip_pr_prin, ip_opr1, ip_opr2, ip_opr3, ip_opr4, ip_opr5,
+     ip_day_pr_prin, ip_day_opr1, ip_day_opr2, ip_day_opr3, ip_day_opr4, ip_day_opr5,
+     ip_mdc, ip_msdrg, grouper_v, ip_sev_code, ip_cat_code
+                           
+         FROM subset_edd_peds")
+  
+  end.time=Sys.time()
+  print(paste('Time Finished: ', end.time))
+  print(end.time-start.time)
+}
--- 1.load_database.R
+++ 1.load_database.R
@@ -1,81 +1,49 @@
-# Parameters----
-
-password = 'ucdavis'
-
-pdd_col_order = read.csv(
-  'local_data/oshpd_full_data/2018-12-07_pdd_column_order_and_types.csv')
-edd_col_order = read.csv(
-  'local_data/oshpd_full_data/2018-12-07_edd_column_order_and_types.csv')
-oshpd_folder = file.path(getwd(),'local_data','oshpd_full_data')
-
-
 #
-# Set-up----
+# Set-up ----
 #
 
+# Import custom functions
+
+source('1.1_supporting_functions.R')
+
+# Import libraries
 library(RPostgres) # database
 library(tidyverse) # includes
 # dplyr(data munging), ggplot2 (plotting), tidyr (tidy data)
 # library(dplyr) # data munging
 
-#
-# Custom functions----
-#
+# Import files with column designations
 
-db_copy_and_subset = function(file, oshpd_folder, 
-                              column_designations,
-                              destination_table = 'pdd_peds',
-                              unified_col_order = "*",
-                              subset_command = "agyradm<27",
-                              diagnostic=TRUE
-){
-  # create new temp table using format from header
-  dbExecute(con, paste("CREATE TABLE temp_table (",column_designations,")"))
-  before.records = dbGetQuery(con, "SELECT COUNT(*) from temp_table")
-  
-  # read data from year----
-  start.time = Sys.time()
-  command = paste0("COPY temp_table FROM '",
-                   file.path(oshpd_folder,file),
-                   "'DELIMITER ',' CSV Header;")
-  if(diagnostic) print(command)
-  dbExecute(con, command)
-  
-  end_time = Sys.time()
-  if(diagnostic) print(end_time - start.time)
-  if(diagnostic) after.records = dbGetQuery(con, "SELECT COUNT(*) from temp_table")
-  if(diagnostic) print(c(before.records, after.records))
-  if(diagnostic) print(list(before=before.records, after=after.records))
-  
-  # copy peds data from year----
-  start.time = Sys.time()
-  before.peds.number = dbGetQuery(
-    con, paste("SELECT COUNT(*) from", destination_table))
-  command = paste("INSERT INTO",destination_table,
-                  "SELECT",unified_col_order,
-                  "FROM temp_table WHERE", subset_command)
-  if (diagnostic) print (command)
-  dbExecute(con, command)
-  end_time = Sys.time()
-  if(diagnostic) print(end_time - start.time)
-  after.peds.number = dbGetQuery(
-    con, paste("SELECT COUNT(*) from", destination_table))
-  if(diagnostic) print(list(before=before.peds.number, after=after.peds.number))
-  
-  # reset/clear temp table----
-  dbExecute(con, "DROP TABLE temp_table")
-}
+oshpd_folder = file.path(getwd(),'local_data','oshpd_full_data')
 
+pdd_col_order = read.csv(
+  file.path(oshpd_folder,'2018-12-07_pdd_column_order_and_types.csv'),
+  header = TRUE, stringsAsFactors = FALSE)
+edd_col_order = read.csv(
+  file.path(oshpd_folder,'2018-12-07_edd_column_order_and_types.csv'),
+  header = TRUE, stringsAsFactors = FALSE)
+combined_peds_cols = read.csv(
+  file.path(oshpd_folder,'2018-12-19_combined_table_designations.csv'), 
+  header=TRUE, stringsAsFactors = FALSE)
+
+# hack - need to rename since '2011' gets read as 'i..2011'
+colnames(pdd_col_order) = c("X2011", "X2011_types", "X2016", "X2016_types")
+colnames(edd_col_order) = c(
+  "X2011", "X2011_types", "X2015", "X2015_types","X2016", "X2016_types"
+)
+colnames(combined_peds_cols) = c(
+  "name", "designation", "sql_command"
+)
 
 #
 # Connect to database----
 #
 
 con = dbConnect(RPostgres::Postgres(),
-                 dbname='oshpd_postgres', 
-                 host="localhost", port=5433,
-                 user='postgres',
-                 password=password)
+                dbname='oshpd_postgres', 
+                host="localhost", port=5433,
+                user='postgres',
+                password='')
 dbListTables(con)
 
 
@@ -83,17 +51,14 @@ dbListTables(con)
 # Set PD table----
 #
 
-pdd_2011_col_types = paste(pdd_col_order$X2011_types, collapse=", ")
-pdd_2011_order = paste(pdd_col_order$X2011, collapse=", ")
-pdd_2016_col_types = paste(pdd_col_order$X2016_types, collapse=", ")
-
-suppress = dbExecute(con, paste("CREATE TABLE pdd_peds (",pdd_2011_col_types,")"))
+pdd_2011_col_types = paste(pdd_col_order$X2011_types, collapse=', ')
+pdd_2011_order = paste(pdd_col_order$X2011, collapse=', ')
+pdd_2016_col_types = paste(pdd_col_order$X2016_types, collapse=', ')
 
 #
 # Copy PDD databases----
 #
 
-dbExecute(con, "DROP TABLE pdd_peds")
 start.time = Sys.time()
 suppress = dbExecute(con, paste("CREATE TABLE pdd_peds (",pdd_2011_col_types,")"))
 
@@ -125,10 +90,10 @@ tbl(con, 'pdd_peds') %>% select(dsch_yr, agyradm) %>% collect() %>% table()
 # Set up ED table----
 #
 
-edd_2011_col_types = paste(edd_col_order$X2011_types, collapse=", ")
-edd_2011_order = paste(edd_col_order$X2011, collapse=", ")
-edd_2015_col_types = paste(edd_col_order$X2015_types, collapse=", ")
-edd_2016_col_types = paste(edd_col_order$X2016_types, collapse=", ")
+edd_2011_col_types = paste(edd_col_order$X2011_types, collapse=', ')
+edd_2011_order = paste(edd_col_order$X2011, collapse=', ')
+edd_2015_col_types = paste(edd_col_order$X2015_types, collapse=', ')
+edd_2016_col_types = paste(edd_col_order$X2016_types, collapse=', ')
 
 suppress = dbExecute(con, paste("CREATE TABLE edd_peds (",edd_2011_col_types,")"))
 
@@ -159,5 +124,143 @@ end.time=Sys.time()
 print(paste('Time Finished: ', end.time))
 print(end.time-start.time)
 tbl(con, 'edd_peds') %>% select(serv_y, agyrserv) %>% collect() %>% table()
-tbl(con, 'edd_peds') %>% select(serv_y, agyrserv) %>% collect() %>% table()
 
+
+#
+# Create combined table -----
+#
+
+# suppress_out = dbExecute(con, "DROP TABLE combined_peds")
+command = paste0(
+  "CREATE TABLE combined_peds(",
+  paste(combined_peds_cols$sql_command, collapse=", "),
+  ")"
+)
+print(command)
+suppress_out = dbExecute(con, command)
+
+# creates a new ID column that automatically generates(in first column)
+suppress_out = dbExecute(con, " CREATE TABLE combined_peds2 
+                         (id serial primary key, like combined_peds INCLUDING ALL)")
+suppress_out = dbExecute(con, "DROP TABLE combined_peds")
+suppress_out = dbExecute(con, 
+                         "ALTER TABLE combined_peds2 RENAME TO combined_peds")
+
+#suppress_out = dbExecute(con, "DROP SEQUENCE record_id_seq")
+suppress_out = dbExecute(con, "CREATE SEQUENCE record_id_seq")
+
+#
+# Add PD datasets to combined table ----
+#
+
+start.time = Sys.time()
+# suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--2011--')
+dbExecute(con, "SELECT * INTO subset_pdd_peds FROM pdd_peds WHERE dsch_yr=2011") 
+recode_add_PDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--2012--')
+dbExecute(con, "SELECT * INTO subset_pdd_peds FROM pdd_peds WHERE dsch_yr=2012") 
+recode_add_PDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--2013--')
+dbExecute(con, "SELECT * INTO subset_pdd_peds FROM pdd_peds WHERE dsch_yr=2013") 
+recode_add_PDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--2014--')
+dbExecute(con, "SELECT * INTO subset_pdd_peds FROM pdd_peds WHERE dsch_yr=2014") 
+recode_add_PDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--2015--')
+dbExecute(con, "SELECT * INTO subset_pdd_peds FROM pdd_peds WHERE dsch_yr=2015") 
+recode_add_PDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--2016--')
+dbExecute(con, "SELECT * INTO subset_pdd_peds FROM pdd_peds WHERE dsch_yr=2016") 
+recode_add_PDD_subset()
+# suppress_out = dbExecute(con, "DROP TABLE subset_pdd_peds")
+
+print('--------------------------------')
+print('All done copying years 2011-2016')
+end.time=Sys.time()
+print(paste('Time Finished: ', end.time))
+print(end.time-start.time)
+
+
+#
+# Add ED datasets to combined table ----
+#
+
+start.time = Sys.time()
+suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--2011--')
+dbExecute(con, "SELECT * INTO subset_edd_peds FROM edd_peds WHERE serv_y=2011") 
+recode_add_EDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--2012--')
+dbExecute(con, "SELECT * INTO subset_edd_peds FROM edd_peds WHERE serv_y=2012") 
+recode_add_EDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--2013--')
+dbExecute(con, "SELECT * INTO subset_edd_peds FROM edd_peds WHERE serv_y=2013") 
+recode_add_EDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--2014--')
+dbExecute(con, "SELECT * INTO subset_edd_peds FROM edd_peds WHERE serv_y=2014") 
+recode_add_EDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--2015--')
+dbExecute(con, "SELECT * INTO subset_edd_peds FROM edd_peds WHERE serv_y=2015") 
+recode_add_EDD_subset()
+suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--2016--')
+dbExecute(con, "SELECT * INTO subset_edd_peds FROM edd_peds WHERE serv_y=2016") 
+recode_add_EDD_subset()
+# suppress_out = dbExecute(con, "DROP TABLE subset_edd_peds")
+
+print('--------------------------------')
+print('All done copying years 2011-2016')
+end.time=Sys.time()
+print(paste('Time Finished: ', end.time))
+print(end.time-start.time)
+
+
+#
+# Validate Combination ----
+#
+
+# view header
+dbGetQuery(con, "SELECT * FROM combined_peds LIMIT 10")
+
+# count by database 
+res = dbGetQuery(
+  con, "SELECT database, count(database) FROM combined_peds GROUP BY database")
+res %>% print()
+#res %>% arrange(database) 
+
+# count by database and year
+res2 = dbGetQuery(
+  con, "
+  SELECT database, end_year, count(database) 
+  FROM combined_peds GROUP BY database, end_year")
+res2 %>% print()
+
+
+# count by database and year
+res3 = dbGetQuery(
+  con, "
+  SELECT database, start_year, count(database) 
+  FROM combined_peds GROUP BY database, start_year")
+res3 %>% print()
